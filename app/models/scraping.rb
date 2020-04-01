e1b8caa57969f80b3a4a3ec9263e489027df9c4e
seima-osako <kirama129@outlook.jp>
2020-04-02 03:42:38 +0900

    追記

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,9 +1,15 @@
 require 'mechanize'
 
 agent = Mechanize.new
-page = agent.get("https://app-mooovi.herokuapp.com/works/initial_scraping")
-elements = page.search('p')
+page = agent.get('https://ytranking.net/?p=1')
+elements = page.search('.channel-list .title')
 
 elements.each do |ele|
   puts ele.inner_text
+end
+
+elements = page.search('.channel-list img')
+
+elements.each do |ele|
+  puts ele.get_attribute('src')
 end
