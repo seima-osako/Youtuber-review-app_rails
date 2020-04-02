90a83f58781f6c7217024f97b790bd48a99d7489
seima-osako <kirama129@outlook.jp>
2020-04-02 19:37:08 +0900

    1対多のアソシエーションの記述

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :product
+
   validates :nickname, presence: true, length: { maximum: 10 }
   validates :review, presence: true, length: { maximum: 1000 }
 end
