9db07e697e5cc5e1e594186b4a3d8e571e28305e
seima-osako <kirama129@outlook.jp>
2020-04-02 15:41:34 +0900

    詳細画面機能追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,7 @@ class ProductsController < RankingController
   end
 
   def show
-    # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
-    @product = Product.new # 問題3ではこのコードは消して新しくコードを書いてください
+    @product = Product.find(params[:id])
   end
 
   def search
